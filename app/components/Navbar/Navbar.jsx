7ecccfd3d37c0b795491688edf97dee723e2f46d
ReVuZ <revathymr916@gmail.com>
2026-01-03 18:22:26 +0530

    Basic Folder Structure and Favicons

---
+++ app/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+function Navbar() {
+  return (
+    <ul>
+      <li><Link href="/">Home</Link></li>
+      <li><Link href="/about">About</Link></li>
+    </ul>
+  )
+}
+
+export default Navbar
